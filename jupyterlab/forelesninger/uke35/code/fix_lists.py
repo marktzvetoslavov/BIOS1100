353691f0dd8cd7ab8004a248e2dfd3553867fed2
marktzvetoslavov <markinternationalbusiness@gmail.com>
2022-09-14 18:31:44 +0200

    uke 35 (code + figures + other files ...)

---
+++ jupyterlab/forelesninger/uke35/code/fix_lists.py
@@ -0,0 +1,4 @@
+list1 = ["Hello", "world, "!"]
+list2 = [1, 2, 5, 12, 18)
+list3 = [0, -1, -2, -4, -5].
+list4 = [a, b, c, d, e]
---
+++ jupyterlab/forelesninger/uke35/code/navn.py
@@ -0,0 +1,6 @@
+biologer = ['Darwin', 'Mendel', 'Fisher', 'Bonnevie', 'Hessen']
+indeks = 0
+while indeks < len(biologer):
+    biologen = biologer[indeks]
+    print(indeks, biologen)
+    indeks = indeks + 1
